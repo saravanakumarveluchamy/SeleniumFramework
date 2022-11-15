f0c37b2b17f2b439b14de63c1c29e6552cc228fe
sarav <sarav@host.docker.internal>
2022-11-15 17:34:35 +0530

    jenkin

--- MavenProject/src/test/java/testpackage/seleniumDockerExecution.java
+++ MavenProject/src/test/java/testpackage/seleniumDockerExecution.java
@@ -56,7 +56,7 @@ public class seleniumDockerExecution {
 		/////WebDriver driver=new RemoteWebDriver(new URL("http://127.0.0.1:58486"),options);
 		
 		 System.setProperty("webdriver.chrome.driver", "C:\\Root\\official\\Selenium_Jar\\chromedriver.exe");  
-         
+         System.out.println("SAmple");
          // Instantiate a ChromeDriver class.     
 		WebDriver driver=new ChromeDriver();  
 		driver.get("http://demo.guru99.com/test/simple_context_menu.html");
