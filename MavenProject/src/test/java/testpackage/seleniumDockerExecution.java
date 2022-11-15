0762f1b5c9a9bd992f138667d895aac9b6554c30
sarav <sarav@host.docker.internal>
2022-11-16 00:13:43 +0530

    test6

--- MavenProject/src/test/java/testpackage/seleniumDockerExecution.java
+++ MavenProject/src/test/java/testpackage/seleniumDockerExecution.java
@@ -52,8 +52,9 @@ public class seleniumDockerExecution {
 		
 		options.merge(capabilities);
 		
+		
 		//ChromeDriver driver = new ChromeDriver(options);
-		WebDriver driver=new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"),options);
+		WebDriver driver=new RemoteWebDriver(new URL("http://172.17.0.3:4444/wd/hub"),options);
 		/////WebDriver driver=new RemoteWebDriver(new URL("http://127.0.0.1:58486"),options);
 		
 		// System.setProperty("webdriver.chrome.driver", "chromedriver.exe");  
